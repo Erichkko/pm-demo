94e9f3fea5bd853845ff7f8c8f20aa834baa1a5c
erichkko <erichkko@163.com>
2018-05-22 19:35:51 +0800

    1.添加侧边栏组件

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -1,15 +1,18 @@
 <template>
 <div>
   <vHeader></vHeader>
+  <vSidebar></vSidebar>
 </div>
 </template>
 
 <script>
   import vHeader from '../common/Header.vue'
+  import vSidebar from '../common/Sidebar.vue'
     export default {
         name: "Home",
         components:{
-        vHeader:vHeader
+        vHeader:vHeader,
+        vSidebar:vSidebar,
       }
     }
 
